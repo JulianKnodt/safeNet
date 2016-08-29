fc2f76c6eb8d18d248f671678fdaac19778faefd
JulianKnodt <julianknodt@gmail.com>
2016-08-29 13:30:06 -0700

    Worked on getting resources and ui-view

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,14 @@
 angular.module('app', [
+  'home',
   'ui.router'
   ])
 .config(function($stateProvider){
   $stateProvider
-  .state('/listening'{
-
+  .state('/listening', {
+    templateUrl: '/home/home.html',
+    controller: 'homeController'
   });
+})
+.run(function($location){
+  $location.path('/home');
 })
