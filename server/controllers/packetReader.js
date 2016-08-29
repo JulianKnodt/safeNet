7e0cebad08a41c859d8ef89831fae1039733650c
JulianKnodt <julianknodt@gmail.com>
2016-08-29 11:55:18 -0700

    Added basic TCP tracker

--- server/controllers/packetReader.js
+++ server/controllers/packetReader.js
@@ -1,9 +1,19 @@
 //Intercepting packets
 var pcap = require('pcap2');
 var session = pcap.createSession('', "tcp");
+var tcpTracker = new pcap.TCPTracker();
+
+
 session.on('packet', function(raw){
   var decoded = pcap.decode.packet(raw);
-    console.log(decoded);
-    // console.log(decoded);
+  tcpTracker.track_packet(decoded);
 });
+
+tcpTracker.on('session', function(session){
+  console.log(session.src, 'and', session.dst);
+  session.on('end', function(session){
+    console.log('end of session from', session.src);
+  });
+});
+
 module.exports = session;
